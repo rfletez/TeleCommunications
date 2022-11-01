b680601b60db1eee2b0017e4d875d9144d0d59cc
Rosny Fletez Jr. <rfletezjr@gmail.com>
2022-11-01 15:19:42 -0700

    First Commit to test the connection to GitHub.

---
+++ call-details/src/main/java/com/commute/calldetails/entity/CallDetails.java
@@ -0,0 +1,10 @@
+package com.commute.calldetails.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "calldetails")
+public class CallDetails {
+
+}
