5a96dbbbfd030ddbb13a1bdd958f77d3f54d08b9
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-02 08:05:52 -0700

    Added the CustomerMS in BackEnd

---
+++ BackEnd/customerMS/src/main/java/com/commute/customerMS/CustomerMsApplication.java
@@ -0,0 +1,13 @@
+package com.commute.customerMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMsApplication.class, args);
+	}
+
+}
---
+++ BackEnd/customerMS/src/test/java/com/commute/customerMS/CustomerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commute.customerMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
