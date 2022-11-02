99df1a9d3e563995edd2c080c23ddffccb13c618
Rosny Fletez Jr. <rfletezjr@gmail.com>
2022-11-02 07:16:44 -0700

    Second Commit from BackEnd. Forgot to add a folder called 'BackEnd' to make it more organized.

---
+++ BackEnd/call-details/src/main/java/com/commute/calldetails/CallDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.commute.calldetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallDetailsApplication.class, args);
+	}
+
+} 
---
+++ BackEnd/call-details/src/main/java/com/commute/calldetails/entity/CallDetails.java
@@ -0,0 +1,11 @@
+package com.commute.calldetails.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "calldetails")
+public class CallDetails {
+
+	
+}
