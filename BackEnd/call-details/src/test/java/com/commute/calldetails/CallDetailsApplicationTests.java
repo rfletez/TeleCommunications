84df3b926750851e3b337d64d18d2eab070cd0e6
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-02 07:26:20 -0700

    Uploading Files from VS Code
    Have to commit from VS Code due to the recent changes of the new folder added.

---
+++ BackEnd/call-details/src/test/java/com/commute/calldetails/CallDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commute.calldetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
