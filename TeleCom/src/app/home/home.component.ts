8d2613e3b357ad9dc99f6935c244bff2e7896a48
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-11-22 10:52:11 -0700

    Finished on the TS file

--- TeleCom/src/app/home/home.component.ts
+++ TeleCom/src/app/home/home.component.ts
@@ -8,11 +8,27 @@ import { TasksService } from './tasks.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  responsiveOptions?: [];
+  responsiveOptions;
   tasks: Task[] = [];
 
   constructor(private taskService: TasksService) {
-    this.responsiveOptions = [];
+    this.responsiveOptions = [
+      {
+        breakpoint: "1024px",
+        numVisible: 3,
+        numScroll: 3,
+      }, 
+      {
+        breakpoint: "768px",
+        numVisible: 2,
+        numScroll: 2
+      }, 
+      {
+        breakpoint: "560px",
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
   }
 
   ngOnInit(): void {
