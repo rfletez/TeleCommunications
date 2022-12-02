70d39462d70344317f900e6c663d0b11bb8da0a9
Rosny Fletez Jr <rfletezjr@gmail.com>
2022-12-02 15:15:09 -0700

    Second Commit, saving changes on CSS file

--- TeleCom/src/app/add-friend/add-friend.component.ts
+++ TeleCom/src/app/add-friend/add-friend.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AddFriendService } from './add-friend.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-friend',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFriendComponent implements OnInit {
 
-  constructor() { }
+  submit: boolean = false;
+  addFriendForm?: FormGroup;
+  errorMessage: string = '';
+  successMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder, 
+    private addFriendService: AddFriendService
+  ) { }
+
+  addFriend(): void {
+    this.submit = true;
+    this.addFriendService.addFriend(this.addFriendForm?.value).subscribe(
+      (data) => {
+        this.successMessage = "Added Successfully";
+        this.errorMessage = '';
+        this.addFriendForm?.reset();
+        setTimeout(() => this.successMessage = '', 3000);
+      },
+      (error) => {
+        this.errorMessage = error;
+        this.submit = false;
+        this.addFriendForm?.reset();
+        setTimeout(() => this.errorMessage = '', 3000);
+      }
+    );
+  }
 
   ngOnInit(): void {
+    this.addFriendForm = this.formBuilder.group({
+      friendAndFamily: [
+        "",
+        [Validators.required, Validators.pattern("[6-9][0-9]{9}")]
+      ]
+    });
   }
 
 }
