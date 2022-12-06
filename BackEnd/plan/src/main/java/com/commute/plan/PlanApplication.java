800aa74a70e63bffcde7429530c38629a7bb724b
Rosny Fletez Jr. <rfletezjr@gmail.com>
2022-12-06 12:36:36 -0700

    Checked over the rest of the MS and are updated.

--- BackEnd/plan/src/main/java/com/commute/plan/PlanApplication.java
+++ BackEnd/plan/src/main/java/com/commute/plan/PlanApplication.java
@@ -2,8 +2,10 @@ package com.commute.plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PlanApplication {
 
 	public static void main(String[] args) {
